cde19299e8f7f32730f89edf4ba32a13a5e067df
Rom Orlovich <romorlovich@gmail.com>
2023-02-17 19:02:51 +0200

    refactor UserQuery component

--- src/components/UserQuery/UserQuery.tsx
+++ src/components/UserQuery/UserQuery.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SelectInput from '../Inputs/SelectInput/SelectInput';
+import { Option } from '../Inputs/SelectInput/selectInput';
 import {
   DISTANCE_OPTIONS,
   EXPERIENCE_OPTIONS,
@@ -13,57 +14,35 @@ const userQueryStyle = {
   optionContainer: 'text-right',
   label: 'text-right'
 };
+
+const selectInputProps = (title: string, options: Option[]) => ({
+  labelProps: {
+    title: title,
+    className: userQueryStyle.label
+  },
+  options: options,
+  optionsElProps: {
+    className: userQueryStyle.optionContainer
+  }
+});
+
 function UserQuery() {
   return (
     <div className="flex flex-col gap-2">
       <div className={userQueryStyle.selectInputsContainer}>
         {/* <SelectInput /> */}
         <SelectInput
-          labelProps={{
-            title: 'ניסיון מקצועי',
-            className: userQueryStyle.label
-          }}
-          options={EXPERIENCE_OPTIONS}
-          optionsElProps={{
-            className: userQueryStyle.optionContainer
-          }}
+          {...selectInputProps('ניסיון מקצועי', EXPERIENCE_OPTIONS)}
         />
       </div>
 
       <div className={userQueryStyle.selectInputsContainer}>
         {/* <SelectInput /> */}
-        <SelectInput
-          labelProps={{
-            title: 'מרחק מהבית',
-            className: userQueryStyle.label
-          }}
-          options={DISTANCE_OPTIONS}
-          optionsElProps={{
-            className: userQueryStyle.optionContainer
-          }}
-        />
+        <SelectInput {...selectInputProps('מרחק מהבית', DISTANCE_OPTIONS)} />
       </div>
       <div className={userQueryStyle.selectInputsContainer}>
-        <SelectInput
-          labelProps={{
-            title: 'סוג עבודה',
-            className: userQueryStyle.label
-          }}
-          options={JOB_TYPES_OPTIONS}
-          optionsElProps={{
-            className: userQueryStyle.optionContainer
-          }}
-        />
-        <SelectInput
-          labelProps={{
-            title: 'סוג משרה',
-            className: userQueryStyle.label
-          }}
-          options={SCOPES_OPTIONS}
-          optionsElProps={{
-            className: userQueryStyle.optionContainer
-          }}
-        />
+        <SelectInput {...selectInputProps('סוג עבודה', JOB_TYPES_OPTIONS)} />
+        <SelectInput {...selectInputProps('סוג משרה', SCOPES_OPTIONS)} />
       </div>
     </div>
   );
