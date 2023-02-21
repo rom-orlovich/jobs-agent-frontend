4f8802d98a0695aaf7fb1dcbaeede591fc57dbb5
Rom Orlovich <romorlovich@gmail.com>
2023-02-21 12:11:51 +0200

    refactor utils.ts in useUserDetailsForm

--- src/hooks/useUserDetailsForm/utils.ts
+++ src/hooks/useUserDetailsForm/utils.ts
@@ -4,25 +4,37 @@ import { GenericRecord } from '@/lib/types/types';
 import { MinMaxInputsOption } from '../../components/UserDetailsForm/Profile/MinMaxSelect';
 import { UserQueryTransform } from './types';
 
-export const transformRequirements = (minMaxValues: MinMaxInputsOption[]) => {
-  const minMaxValuesObj: Requirements = {};
+/**
+ * @param {MinMaxInputsOption[]} minMaxValues The values of minMax inputs.
+ * @returns {Requirements} The transform values of minMax inputs as requirements format.
+ */
+export function transformRequirements(minMaxValues: MinMaxInputsOption[]): Requirements {
+  const requirements: Requirements = {};
   for (const minMaxValue of minMaxValues) {
-    minMaxValuesObj[minMaxValue.field] = {
+    requirements[minMaxValue.field] = {
       min: minMaxValue.min,
       max: minMaxValue.max
     };
   }
-  return minMaxValuesObj;
-};
-export const transformExcludedRequirements = (inputBucketValues: string[]) => {
+  return requirements;
+}
+/**
+ *  @param {string[]} inputBucketValues The values of inputBucket.
+ * @returns {GenericRecord<boolean>} The transform values of inputBucket as excludedRequirements format.
+ */
+export function transformExcludedRequirements(inputBucketValues: string[]): GenericRecord<boolean> {
   const excludedRequirement: GenericRecord<boolean> = {};
   for (const value of inputBucketValues) {
     excludedRequirement[value] = true;
   }
   return excludedRequirement;
-};
+}
 
-export const transformRequirementsDefaultValues = (requirements: Requirements): MinMaxInputsOption[] => {
+/**
+ * @param {Requirements} requirements The requirements object from the DB.
+ * @returns { MinMaxInputsOption[]} The transform values of requirements as MinMaxInputsOption[]
+ */
+export function transformRequirementsDefaultValues(requirements: Requirements): MinMaxInputsOption[] {
   const requirementsEntries = Object.entries(requirements);
   const minMaxSelectOptions = requirementsEntries.map(([key, value]) => ({
     field: key,
@@ -30,24 +42,37 @@ export const transformRequirementsDefaultValues = (requirements: Requirements): 
     max: value?.max || 1
   }));
   return minMaxSelectOptions;
-};
-export const transformExcludedRequirementsDefaultValues = (
+}
+
+/**
+ * @param {GenericRecord<boolean>} excludedRequirements The excludedRequirements object from the DB.
+ * @returns { string[]} The transform values of excludedRequirements as string[]
+ */
+
+export function transformExcludedRequirementsDefaultValues(
   excludedRequirements: GenericRecord<boolean>
-): string[] => {
+): string[] {
   const requirementsEntries = Object.keys(excludedRequirements);
   const minMaxSelectOptions = requirementsEntries.map((key) => key);
   return minMaxSelectOptions;
-};
+}
 
-export const transformUserQuery = (userQuery: UserQuery) => {
+/**
+ * @param {UserQuery} userQuery The userQuery object from the DB.
+ * @returns {UserQueryTransform} The transform values of userQuery as UserQueryTransform
+ */
+export function transformUserQuery(userQuery: UserQuery): UserQueryTransform {
   const transformUserQuery: GenericRecord<string | string[]> = {};
   for (const [key, value] of Object.entries(userQuery)) {
     const joinValue = value?.split(',');
     transformUserQuery[key] = joinValue;
   }
   return transformUserQuery as UserQueryTransform;
-};
+}
 
+/**
+ *  This function enables the form use the values from the DB as inputs' default values.
+ */
 export const transformDefaultFormValues = ({
   requirements,
   excludedRequirements,
