12b1f24526e7bc43ebf1a2a36b5bffe67dd6122a
Rom Orlovich <romorlovich@gmail.com>
2023-02-23 11:53:47 +0200

    create hamburgerMenu- x animation

--- src/components/Layout/Dashbord/Sidebar/Sidebar.tsx
+++ src/components/Layout/Dashbord/Sidebar/Sidebar.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import HamburgerMenu from '../../Navabar/HamburgerMenu/HamburgerMenu';
 
 function Sidebar() {
-  return <section className="fixed top-0 h-full bg-nav-500 shadow-lg">Sidebar</section>;
+  return (
+    <section className="fixed top-0 flex h-full min-w-[4rem] flex-col items-center divide-y-2 bg-nav-500 shadow-lg">
+      <HamburgerMenu />
+    </section>
+  );
 }
 
 export default Sidebar;
--- src/components/Layout/Navabar/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/Layout/Navabar/HamburgerMenu/HamburgerMenu.tsx
@@ -2,17 +2,36 @@ import React, { useState } from 'react';
 
 function HamburgerMenu() {
   const [state, setState] = useState(false);
+
+  const hamburgerMenuStyle = {
+    x: {
+      true: {
+        first: 'translate-y-[2px] rotate-[45deg]',
+        sec: 'rotate-[-45deg]',
+        third: 'hidden'
+      },
+      false: {
+        first: 'translate-y-[-8px] rotate-[0deg] ',
+        sec: 'rotate-[0deg] ',
+        third: 'translate-y-[8px] rotate-[0deg]'
+      }
+    }
+  };
+  const boolStr = String(state) as 'true' | 'false';
+  const menu = hamburgerMenuStyle['x'][boolStr];
   return (
-    <button
-      onClick={() => {
-        setState((pre) => !pre);
-      }}
-      className={`relative ${state ? 'hamburger-menu-close' : ''} space-y-2`}
-    >
-      <div className="first h-0.5 w-8 bg-white"></div>
-      <div className=" third h-0.5 w-8  bg-white"></div>
-      <div className="sec h-0.5 w-8  bg-white"></div>
-    </button>
+    <div className="mt-4 flex h-8 w-full flex-col items-center justify-center">
+      <button
+        onClick={() => {
+          setState((pre) => !pre);
+        }}
+        className={` ${state ? '' : ''} h-4 w-fit `}
+      >
+        <div className={`h-0.5 w-6 bg-white duration-100 ${menu.first}`} />
+        <div className={`h-0.5 w-6 bg-white duration-100 ${menu.sec}`} />
+        <div className={`h-0.5 w-6 bg-white duration-100 ${menu.third}`} />
+      </button>
+    </div>
   );
 }
 
