e7d955fcc5c482ca9e6ae12304ee4396daab6b60
Rom Orlovich <romorlovich@gmail.com>
2023-02-23 13:41:30 +0200

    create hover effect on sidebar nav item

--- src/components/Buttons/ButtonAuth.tsx
+++ src/components/Buttons/ButtonAuth.tsx
@@ -1,9 +1,12 @@
 import useAuth from '@/hooks/useAuth';
 import { BoolKey } from '@/lib/types/types';
+import { classNameGenerator } from '@/lib/utils';
 import { signIn, signOut } from 'next-auth/react';
-import React, { MouseEventHandler } from 'react';
+import React, { MouseEventHandler, ReactNode } from 'react';
+import { ButtonProps } from '../HTML.types';
 
-function ButtonAuth() {
+function ButtonAuth(buttonProps: ButtonProps & { icon?: ReactNode; isOn?: boolean }) {
+  const { icon, isOn } = buttonProps;
   const { isAuthenticated } = useAuth();
   const handleSignOut: MouseEventHandler<HTMLButtonElement> = () => signOut();
   const handleSignIn: MouseEventHandler<HTMLButtonElement> = () => signIn();
@@ -19,9 +22,31 @@ function ButtonAuth() {
   };
   const curButtonOptions = buttonOptions[String(isAuthenticated) as BoolKey];
 
-  return (
-    <button className="button-custom  text-white hover:opacity-75" onClick={curButtonOptions.onClick}>
+  const isOnButtonContent = (
+    <div className="flex items-center gap-1">
+      {icon && icon}
       {curButtonOptions.text}
+    </div>
+  );
+  const isOffButtonContent = icon ? (
+    <div className="group relative">
+      {icon}
+      <span className="absolute top-0 hidden translate-x-[0px] rounded-md bg-white px-2 text-lg text-black shadow-lg duration-100 group-hover:block group-hover:translate-x-[-50px] group-hover:transition">
+        {curButtonOptions.text}
+      </span>
+    </div>
+  ) : (
+    curButtonOptions.text
+  );
+
+  const curButtonContent = isOn ? isOnButtonContent : isOffButtonContent;
+  return (
+    <button
+      {...buttonProps}
+      className={classNameGenerator('button-custom  text-white hover:opacity-75', buttonProps.className)}
+      onClick={curButtonOptions.onClick}
+    >
+      {curButtonContent}
     </button>
   );
 }
--- src/components/Layout/Dashbord/Sidebar/Sidebar.tsx
+++ src/components/Layout/Dashbord/Sidebar/Sidebar.tsx
@@ -5,7 +5,8 @@ import { classNameGenerator } from '@/lib/utils';
 import Link from 'next/link';
 import React, { ReactNode } from 'react';
 import HamburgerMenu from '../../Navabar/HamburgerMenu/HamburgerMenu';
-import Profile from './Profile/Profile';
+import Profile from './Profile';
+import { BiLogOutCircle } from 'react-icons/bi';
 
 const sideBarStyle = {
   nav: 'fixed top-0 flex h-full  flex-col items-center bg-nav-500 shadow-lg',
@@ -16,9 +17,11 @@ const sideBarStyle = {
       false: 'min-w-[4rem]'
     }
   },
+  'hover-link': 'hover:bg-nav-600',
   margin: 'mb-2',
   links: 'mt-2 flex flex-col items-center gap-8',
-  link: ''
+  link: '',
+  icon: 'text-2xl'
 };
 interface NavLinkProps {
   link: string;
@@ -54,16 +57,21 @@ function Sidebar() {
           <section className={classNameGenerator(sideBarStyle.nav, navIsOn, 'duration-500')}>
             <HamburgerMenu {...toggleProps} />
             {toggleProps.isON && <Profile />}
-            <ul className={sideBarStyle.links}>
-              {navLinks.map((el, i) => {
-                return (
-                  <li key={el.text + i}>
-                    <Link href={el.link}>{el.text}</Link>
-                  </li>
-                );
-              })}
-            </ul>
-            <ButtonAuth />
+            <div className="flex h-full flex-col justify-between py-4">
+              <ul className={sideBarStyle.links}>
+                {navLinks.map((el, i) => {
+                  return (
+                    <li key={el.text + i}>
+                      <Link href={el.link}>{isON ? el.icon : el.text}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+              <ButtonAuth
+                className={classNameGenerator(sideBarStyle['hover-link'])}
+                icon={<BiLogOutCircle className={sideBarStyle.icon} />}
+              />
+            </div>
           </section>
         );
       }}
