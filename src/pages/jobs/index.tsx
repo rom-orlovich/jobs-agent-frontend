89b67c25b6e23b2341f4167ccf9d0e63026a955d
Rom Orlovich <romorlovich@gmail.com>
2023-02-24 01:24:40 +0200

    style the jobs page

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -22,16 +22,19 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 };
 function Jobs({ jobs }: InferGetStaticPropsType<typeof getServerSideProps>) {
   return (
-    <ul className="flex flex-wrap justify-center gap-2 p-4">
-      {jobs?.map((el) => {
+    <ul dir="ltr" className="flex flex-wrap justify-center gap-2 p-8">
+      {jobs?.map((el, i) => {
         return (
-          <li className="rounded-md bg-white p-4 shadow-md" key={el.jobID}>
+          <li
+            className="rounded-md bg-white p-4 shadow-md sm:flex-[50%] md:flex-[30%]"
+            key={el.jobID + i}
+          >
             <div>
               <Link href={el.link}> {el.title}</Link>{' '}
             </div>
 
             <div> {el.from} </div>
-            <div> {el.reason} </div>
+            <div> {el.reason || 'jobs is match'} </div>
           </li>
         );
       })}
