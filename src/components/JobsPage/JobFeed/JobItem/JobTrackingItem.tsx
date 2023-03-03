eb48e0f792fdfd6f24b563aaee4b2acdb852bb0c
Rom Orlovich <romorlovich@gmail.com>
2023-03-03 15:53:30 +0200

    add spacebetween the button and the content in jobTrackingItem

--- src/components/JobsPage/JobFeed/JobItem/JobTrackingItem.tsx
+++ src/components/JobsPage/JobFeed/JobItem/JobTrackingItem.tsx
@@ -1,6 +1,6 @@
 import { Job } from '@/lib/types/jobsScanner.types';
 import { APP_ROUTES } from '@/lib/routes';
-import { createLocalDate } from '@/lib/utils';
+import { classNameGenerator, createLocalDate } from '@/lib/utils';
 
 import Link from 'next/link';
 import React, { MouseEventHandler } from 'react';
@@ -18,16 +18,19 @@ function JobTrackingItem(
 ) {
   const jobTrackingItemStyle = {
     ...jobItemStyle,
+    itemTracking: 'flex flex-col justify-between gap-2',
     dateContainer: 'flex flex-end',
-    buttonStatusContainer: 'flex justify-center mt-3',
     sendCVContainer: 'flex flex-end',
     linkTrackDetailsContainer: 'flex justify-center ',
-    linkTrackDetails: 'button-custom bg-status-400 text-white text-sm  flex items-center gap-1'
+    linkTrackDetails: 'button-custom  bg-status-400 text-white text-sm  flex items-center gap-1'
   };
   const { link, title, jobID, index, mark, info } = props;
   const localDateStr = createLocalDate(info?.createdAt);
   return (
-    <li className={jobTrackingItemStyle.item} key={jobID + index}>
+    <li
+      className={classNameGenerator(jobTrackingItemStyle.item, jobTrackingItemStyle.itemTracking)}
+      key={jobID + index}
+    >
       <div className={jobTrackingItemStyle.content}>
         <div className={jobTrackingItemStyle.bookmarkContainer}>
           <TrackButton onClick={props.handleClickBookmark} mark={mark} />
