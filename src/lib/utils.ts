1dee53227580f6277b6d43b05a0fb1805e089a97
Rom Orlovich <romorlovich@gmail.com>
2023-03-03 09:10:19 +0200

    delete unused function in lib/utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@
 import { SERVER_URL } from './endpoints';
 import { MESSAGES, MESSAGE_CODES } from './messages';
 import { ResponseMessage } from './types/api.types';
-import { AnyFun, GenericRecord, OmitKey } from './types/types';
+import { AnyFun, GenericRecord } from './types/types';
 
 /**
  *
@@ -26,42 +26,7 @@ export function capitalFirstLetter(words?: string | null): string | undefined | 
         .join(' ')
     : words;
 }
-
-export const fetchData = async <D>(url: string) => {
-  try {
-    const result = await fetch(url);
-    const data = await result.json();
-    return data as D;
-  } catch (error) {
-    console.log(error);
-    return undefined;
-  }
-};
-
 /**
- *
- * @param {V} obj Object you want to extract the exist key value.
- * @returns {{existKeysValues:Partial<V>,count:number}} The object that include the exist keysValues and count how many exist.
- */
-export function getObjExistKeysValues<V extends GenericRecord<any>>(
-  obj: V
-): { existKeysValues: Partial<V>; count: number } {
-  const existKeysValues = {} as Partial<V>;
-  let count = 0;
-
-  Object.entries(obj).forEach(([key, value]) => {
-    if (key && value) {
-      (existKeysValues[key as keyof V] = value), count++;
-    }
-  });
-  return {
-    existKeysValues,
-    count
-  };
-}
-
-/**
- *
  * @param {T} obj The URL queries string.
  * @param {string} keyValueDelimiter The delimiter the connect between the  key and its value. default "=".
  * @param {string} objFieldDelimiter The delimiter the connect between each key.. default "=". default "&".
@@ -97,7 +62,6 @@ export const createURLPath = (resources: string[]) => {
  * @param objFieldDelimiter  The delimiter the connect between each query's keys. default "=". default "&".
  * @returns The value of the url.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const createURL = <T extends GenericRecord<any>>(
   resources: string[],
   params?: T,
@@ -117,40 +81,6 @@ export const delayFun = (cb: AnyFun, delay: number) =>
       res(await cb());
     }, delay)
   );
-
-/**
- *
- * @param {string} url URL of the request.
- * @param {OmitKey<RequestInit, 'body'> & { body?: Req } | undefined} request Request Object with generic body object
- * @returns {Promise<Res | undefined>} Result data of the mutation fetch.
- */
-export async function fetchUtil<
-  Req extends GenericRecord<any> | undefined,
-  Res extends GenericRecord<any>
->(url: string, request?: OmitKey<RequestInit, 'body'> & { body?: Req }): Promise<Res | undefined> {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const headers = request?.headers;
-  const headerBody = request?.body
-    ? {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(request.body)
-      }
-    : {};
-
-  try {
-    const data = await fetch(url, {
-      method: request?.method,
-      ...headers,
-      ...headerBody
-    });
-    return await data.json();
-  } catch (error) {
-    console.log(error);
-    return undefined;
-  }
-}
 /**
  *
  * @param {KeyCode} keyCode The key of the name of the message.
@@ -165,7 +95,6 @@ export const getResMessage = <KeyCode extends keyof typeof MESSAGE_CODES>(
     code: code
   };
 };
-export const fetchSWR = (url: string) => fetch(url).then((res) => res.json());
 
 export const createScannerURL = (endpoint: string, userID?: string) =>
   createURL([SERVER_URL, endpoint, userID || '']);
