4be5574404a8cabb8fa52c7c4aabf8dbc16c2a89
Rom Orlovich <romorlovich@gmail.com>
2023-03-03 11:56:34 +0200

    delete unused line of code

--- src/components/JobsPage/Jobs.tsx
+++ src/components/JobsPage/Jobs.tsx
@@ -5,19 +5,15 @@ import { ResponseGetJobs } from '@/lib/types/jobsScanner.types';
 import { useSWRInfiniteHook } from '@/lib/swr';
 
 import React from 'react';
-
-// import LoadButton from '../Buttons/LoadButton';
 import Spinner from '../Spinner/Spinner';
 import JobsFeed from './Jobs/JobsFeed';
-// import JobsSearch from './JobsSearch/JobsSearch';
+
 import LoadButtonContainer from './Jobs/LoadButtonContainer';
 import JobsHeader from './Jobs/JobsHeader';
 
 const JobsStyle = {
   feedContainer: 'pr-10 xs:pr-16',
-  jobsHeaderContainer: 'flex justify-between xs:flex-col flex-col',
-  loadButtonContainer: 'flex w-full items-center justify-center',
-  loadButton: 'items-center px-7 py-2 text-2xl',
+
   spinner: '!top-[none] bottom-5'
 };
 
@@ -53,22 +49,8 @@ function Jobs({
   const { allResponseData, lastResponse } = getLastCurJobData(data);
   const jobsData = allResponseData.map((response) => response.jobs).flat(1);
 
-  // const handleLoadButtonClick: MouseEventHandler<HTMLButtonElement> = () => setSize(size + 2);
-
-  // const textHeader = isMatchPage ? 'התאמות' : 'משרות';
-
   return (
     <div className={JobsStyle.feedContainer}>
-      {/* <div className={JobsStyle.jobsHeaderContainer}>
-        <h1 className="text-3xl">
-          כ- {lastResponse.pagination?.numResultsFound || 0} {textHeader} נמצאו:
-        </h1>
-        <JobsSearch
-          filterJobsProps={filterJobsProps}
-          jobsFilters={lastResponse.filters}
-          includeReasonFilter={!isMatchPage}
-        />
-      </div> */}
       <JobsHeader
         filterJobsProps={filterJobsProps}
         filters={lastResponse.filters}
@@ -80,15 +62,6 @@ function Jobs({
 
       {jobsData.length && (
         <LoadButtonContainer setSize={setSize} hasMore={lastResponse.pagination.hasMore} />
-        // <div className={JobsStyle.loadButtonContainer}>
-        //   <LoadButton
-        //     disabled={!lastResponse?.pagination?.hasMore}
-        //     className={JobsStyle.loadButton}
-        //     onClick={handleLoadButtonClick}
-        //   >
-        //     טען משרות
-        //   </LoadButton>
-        // </div>
       )}
 
       <Spinner className={JobsStyle.spinner} isLoading={isValidating || isLoading || !data} />
