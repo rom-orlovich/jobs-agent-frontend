ac8259ec80d26b08f55d2cbdbea71a8724d72977
Rom Orlovich <romorlovich@gmail.com>
2023-03-07 22:52:21 +0200

    delete unused line of code in side bar

--- src/components/Layout/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/Layout/Dashboard/Sidebar/Sidebar.tsx
@@ -8,12 +8,7 @@ import HamburgerMenu from './HamburgerMenu';
 import Profile from './Profile';
 import { BiLogOutCircle } from 'react-icons/bi';
 import SidebarItemContent from './SideNavItem/SidebarItemContent';
-// import { LINKS } from './lib/links';
-// import { useAuthContext } from '@/context/AuthContext';
-// import { useRouter } from 'next/router';
-// import { useScannerContext } from '@/context/ScannerContext';
-// import { toast } from 'react-toastify';
-// import SidebarItem from './SideNavItem/SidebarItem';
+
 import SideLinksItems from './SidebarItems';
 const sideBarStyle = {
   nav: 'fixed z-50 top-0 flex h-[100vh] flex-col items-center bg-nav-500 shadow-lg ',
@@ -25,42 +20,16 @@ const sideBarStyle = {
   },
   hoverLink: 'hover:bg-nav-600',
   'links&button-container': 'flex h-full flex-col justify-between py-4 w-[100%]',
-  links: 'mt-2 flex flex-col items-center gap-4 w-full',
-  linksOffMode: 'h-[70%]  gap-8 justify-center',
   icon: 'text-2xl'
 };
 
 function Sidebar() {
-  // const { userProfileData } = useAuthContext();
-  // const { scanner } = useScannerContext();
-  // const router = useRouter();
-  // const hash = userProfileData.activeHash;
-
   return (
     <Toggle>
       {(toggleProps) => {
         const { isON } = toggleProps;
         const bool = String(isON) as BoolKey;
         const navIsOn = sideBarStyle['isOn']['nav'][bool];
-        // const linksItems = LINKS(sideBarStyle.icon);
-
-        // const sharedQueriesParams = {
-        //   hash: hash,
-        //   page: 1
-        // };
-        //Set jobs page's and jobs matches page's link to be with user's current active hash
-        // linksItems[1].link = createURL([linksItems[1].link], sharedQueriesParams);
-        // linksItems[2].link = createURL([linksItems[2].link], {
-        //   ...sharedQueriesParams,
-        //   reason: 'match'
-        // });
-
-        // const disableByMutateHandler = (e: MouseEvent) => {
-        //   if (scanner.isMutating) {
-        //     e.preventDefault();
-        //     toast(getResMessage('SEARCH_IS_IN_PROCESS').message);
-        //   }
-        // };
 
         return (
           <section className={classNameGenerator(sideBarStyle.nav, navIsOn, 'duration-500')}>
@@ -68,25 +37,7 @@ function Sidebar() {
             {isON && <Profile />}
             <div className={sideBarStyle['links&button-container']}>
               <SideLinksItems isON={isON} />
-              {/* <ul
-                className={classNameGenerator(
-                  sideBarStyle.links,
-                  classIsOn(!isON, sideBarStyle.linksOffMode)
-                )}
-              >
-                {linksItems.map((el, i) => {
-                  return (
-                    <SidebarItem
-                      isMutate={(i === 1 || i === 2) && scanner.isMutating}
-                      isON={isON}
-                      onClickMutate={disableByMutateHandler}
-                      isActiveLink={router.pathname === el.link}
-                      key={el.link + i}
-                      {...el}
-                    />
-                  );
-                })}
-              </ul> */}
+
               <ButtonAuth className={classNameGenerator(sideBarStyle.hoverLink)}>
                 <SidebarItemContent
                   icon={<BiLogOutCircle className={sideBarStyle.icon} />}
