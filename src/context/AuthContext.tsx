812ffe0f7a740a9456c5f8ed15fef60486500508
Rom Orlovich <romorlovich@gmail.com>
2023-03-07 20:01:45 +0200

    refactor the Layout component

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,36 @@
-import { ReturnTypeUseAuthProfileExist } from '@/hooks/useAuth';
+import Spinner from '@/components/Spinner/Spinner';
+import useAuth, { ReturnTypeUseAuthProfileExist } from '@/hooks/useAuth';
 import React, { createContext, PropsWithChildren, useContext } from 'react';
 
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin']
+});
+import { Roboto } from '@next/font/google';
+
 export const context = createContext({} as ReturnTypeUseAuthProfileExist);
-//
-function AuthContext(props: { authData: ReturnTypeUseAuthProfileExist } & PropsWithChildren) {
-  return <context.Provider value={props.authData}>{props.children}</context.Provider>;
+
+//Create the auth provider.
+function AuthContext(props: PropsWithChildren) {
+  const auth = useAuth();
+  const Main = ({ children }: PropsWithChildren) => (
+    <main className={roboto.className + ' ' + 'bg-background' + ' min-h-[100vh]'}>{children}</main>
+  );
+  const { isAuthenticated, isLoading, isValidating } = auth;
+
+  if (!isAuthenticated) return <></>;
+  if (isLoading || isValidating)
+    return (
+      <Main>
+        <Spinner isLoading={isLoading || isValidating} />;
+      </Main>
+    );
+
+  return (
+    <Main>
+      <context.Provider value={auth}>{props.children}</context.Provider>;
+    </Main>
+  );
 }
 
 /**
