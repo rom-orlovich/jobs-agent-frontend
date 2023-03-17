524cf4c9578267edc71661deb30774455e7c466c
Rom Orlovich <romorlovich@gmail.com>
2023-03-17 21:55:07 +0200

    finish create genric SearchInput component

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReturnUseFilterProps } from '@/hooks/useFilters';
+import { GenericRecord } from '@/lib/types/types';
+import React, { PropsWithChildren } from 'react';
+import { BiSearch } from 'react-icons/bi';
+import Autocomplete from '../Inputs/Autocomplete/Autocomplete';
+import { Option } from '../Inputs/SelectInput/selectInput.types';
 
-function SearchInput() {
-  return <div>SearchInput</div>;
+export const jobsSearchStyle = {
+  jobSearchContainer: 'flex xs:justify-start w-full',
+  autocompleteWrapper: 'relative w-fit items-center',
+  input: 'py-1 min-w-[15rem]',
+  icon: 'text-blue-300 absolute right-2  text-xl top-[25%]',
+  filterIcon: 'text-filter-400 hover:text-filter-500 ml-1 text-2xl'
+};
+
+export const IconButtonProps = {
+  Icon: <BiSearch />,
+  buttonProps: {
+    className: jobsSearchStyle.icon
+  }
+};
+
+/**
+ * @param FV The filters hook inputs values props.
+ * @param HRV The filter hook's return values
+ */
+interface MainSearchInputProps<FV extends GenericRecord<any>, HRV extends ReturnUseFilterProps<FV>> {
+  filterJobsProps: HRV;
+  defaultValue?: Option<string>;
+  key: keyof ReturnUseFilterProps<FV>['formValues'];
+  options: Option<string>[];
+}
+function SearchInput<FV extends GenericRecord<any>, HRV extends ReturnUseFilterProps<FV>>({
+  mainSearchInputProps,
+  children
+}: {
+  mainSearchInputProps: MainSearchInputProps<FV, HRV>;
+} & PropsWithChildren) {
+  const { filterJobsProps, key, defaultValue, options } = mainSearchInputProps;
+
+  return (
+    <div dir="ltr" className={jobsSearchStyle.jobSearchContainer}>
+      <Autocomplete
+        defaultValue={defaultValue}
+        setValue={filterJobsProps.handleSearchValue(key)}
+        inputLabelProps={{
+          IconButtonProps: IconButtonProps,
+          inputProps: {
+            className: jobsSearchStyle.input
+          },
+          wrapperInputLabel: {
+            className: jobsSearchStyle.autocompleteWrapper
+          }
+        }}
+        options={options}
+      />
+      {children}
+      {/* <JobsSearchFilters
+        includeReasonFilters={includeReasonFilters}
+        jobsFilters={jobsFilters}
+        iconButtonProps={{
+          ...IconButtonProps
+        }}
+        filterJobsProps={filterJobsProps}
+      /> */}
+    </div>
+  );
 }
 
 export default SearchInput;
