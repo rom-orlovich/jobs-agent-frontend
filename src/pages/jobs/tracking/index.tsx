0e5e7c24fc74580b0cdcc8b2904c0a2197109644
Rom Orlovich <romorlovich@gmail.com>
2023-03-17 22:56:42 +0200

    delete unused lines of code in jobs/tracking/index.tsx

--- src/pages/jobs/tracking/index.tsx
+++ src/pages/jobs/tracking/index.tsx
@@ -10,26 +10,13 @@ import PageHead from '@/components/Layout/PageHead/PageHead';
 import React from 'react';
 
 function Track() {
-  // //Get user profile data.
-  // const { userProfileData } = useAuthContext();
-
-  // //Check if the the exist and redirect to home page and display proper message.
-  // useRedirect(
-  //   createToastsByDataIfExist(
-  //     'TRACKING_JOBS_ARE_FOUND',
-  //     'TRACKING_JOBS_ARE_NOT_FOUND',
-  //     userProfileData?.tracking
-  //   )
-  // );
   return (
     <>
       <PageHead
         title="Jobs Tracking"
         description="Here is the place to find all the jobs which are in your tracking."
       />
-      {/* <div className="pl-4 pr-12 xs:pr-20">
-        <JobsFeed userProfileData={userProfileData} isTrackingFeed={true} />
-      </div> */}
+
       <JobTrackingPage />
     </>
   );
