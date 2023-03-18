933887b32d58c8000cd05b3c65581cba9375c8b4
Rom Orlovich <romorlovich@gmail.com>
2023-03-18 19:16:32 +0200

    refactor JobTrackingPage/utils.ts

--- src/components/JobTrackingPage/utils.ts
+++ src/components/JobTrackingPage/utils.ts
@@ -2,11 +2,14 @@ import { JobsTrackingFiltersFields } from '@/hooks/useFiltersHooks/useFiltersTra
 import { Job } from '@/lib/types/jobsScanner.types';
 
 /**
- * @param {JobsTrackingFiltersFields} filterValues The current jobs tracking's filter values fields
- * @param {Jobs[] | undefined} jobs The current jobs tracking that suppose to be filtered
- * @returns {Jobs[] | undefined} The current jobs tracking filter by filter values fields.
+ * @param {JobsTrackingFiltersFields} filterValues The current jobs tracking's filter values fields.
+ * @param {Job[] | undefined} jobs The current jobs tracking that suppose to be filtered
+ * @returns {Job[] | undefined} The current jobs tracking filter by filter values fields.
  */
-export const filtersJobsTracking = (filterValues: JobsTrackingFiltersFields, jobs?: Job[]) => {
+export const filtersJobsTracking = (
+  filterValues: JobsTrackingFiltersFields,
+  jobs?: Job[]
+): Job[] | undefined => {
   let currentJobs = jobs;
   const { title, CVwasSent, afterUpdateDate, currentStageName } = filterValues;
 
@@ -31,26 +34,32 @@ export const filtersJobsTracking = (filterValues: JobsTrackingFiltersFields, job
 };
 
 /**
- * @param {Jobs[] | undefined} jobs The current tracking jobs that suppose to be sorted
- * @returns {Jobs[] | undefined} The current jobs tracking sort by their updatedAt field.
+ * @param {Job[] | undefined} jobs The current jobs tracking that suppose to be sorted
+ * @returns {Job[] | undefined} The current jobs tracking sort by their updatedAt field.
  */
-export const sortJobsTrackingByCreatedDate = (jobs?: Job[]) => {
+export const sortJobsTrackingByCreatedDate = (jobs?: Job[]): Job[] | undefined => {
   return jobs?.sort(
     (a, b) => new Date(b.info?.updatedAt || '').getTime() - new Date(a.info?.updatedAt || '').getTime()
   );
 };
 
-export const createJobsTrackingFiltersArrValues = (jobs?: Job[]) => {
-  const titles: Map<string, string> = new Map([]);
-  const currentStageNames: Map<string, string> = new Map([]);
+/**
+ * @param {Job[]} jobs The current jobs tracking.
+ * @returns { {titles:string[],currentStageNames:string[]}} An object of unique string arrays of titles and currentStageNames of all the user's jobs tracking data.
+ */
+export const createJobsTrackingFiltersArrValues = (
+  jobs?: Job[]
+): { titles: string[]; currentStageNames: string[] } => {
+  const titles: Set<string> = new Set([]);
+  const currentStageNames: Set<string> = new Set([]);
   jobs?.forEach((job) => {
     const curStageName = job.info?.stages.at(-1)?.name;
-    if (job.title) titles.set(job.title, job.title);
-    if (curStageName) currentStageNames.set(curStageName, curStageName);
+    if (job.title) titles.add(job.title);
+    if (curStageName) currentStageNames.add(curStageName);
   });
 
   return {
-    titles: [...titles.values()],
-    currentStageNames: [...currentStageNames.values()]
+    titles: [...titles],
+    currentStageNames: [...currentStageNames]
   };
 };
