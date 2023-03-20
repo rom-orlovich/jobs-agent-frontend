69790ba52a393aec2c7046a1d373f17c185fd4e7
Rom Orlovich <romorlovich@gmail.com>
2023-03-20 17:34:14 +0200

    imporve the text info of RequirementsInfo.tsx and ExcludedRequirementsInfo.tsx

--- src/components/UserProfileForm/InfoTexts/ExcludedRequirementsInfo.tsx
+++ src/components/UserProfileForm/InfoTexts/ExcludedRequirementsInfo.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function ExcludedRequirementsInfo() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <p>
         מונחים <b> שאינך מעוניינ\ת</b> שיופיעו בתיאור המשרות.
       </p>
       <p>
-        <b>דיוק במונחים אלו, </b> ישפר את סיכויי ההתאמה של המשרות לפרופיל שלך.
+        <b>דיוק הדרישות</b> לאחר כל חיפוש, יגדיל את <b>סיכויי ההתאמה</b> של המשרות לפרופיל האישי שלך.
       </p>
     </div>
   );
